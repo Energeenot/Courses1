57356385e55b093903299593976207697ea8ab6b
кпк <abramov.pandos@yandex.ru>
2020-04-05 01:20:16 +0300

    пример восходящего и низходящего преобразования

---
+++ src/late_and_early_binding_example/Animal.java
@@ -0,0 +1,7 @@
+package late_and_early_binding_example;
+
+public class Animal {
+    public void live(){
+        System.out.println("I can live");
+    }
+}
---
+++ src/late_and_early_binding_example/Person.java
@@ -0,0 +1,7 @@
+package late_and_early_binding_example;
+
+public class Person extends Animal{
+    public void to_reason(){
+        System.out.println("I can reason");
+    }
+}
